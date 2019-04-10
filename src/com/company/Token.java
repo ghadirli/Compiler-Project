c0017cb15b1bc5e244b0b7db5b48e933617fabbf
Soroush <soroushtaslimi@yahoo.com>
2019-04-10 13:55:44 +0430

    Token class added

---
+++ src/com/company/Token.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Token {
+    String description;
+    TokenTypes tokenTypes;
+
+
+}
+
+enum TokenTypes{
+    ID, NUM, KEYWORD, EOF, SYMBOL, COMMENT, WHITESPACE
+}
