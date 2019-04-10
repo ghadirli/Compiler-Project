f472f020056b17c1f657ebe4ab2300c5b571aa68
Soroush <soroushtaslimi@yahoo.com>
2019-04-10 14:41:00 +0430

    token types initialized

--- src/com/company/Lexer.java
+++ src/com/company/Lexer.java
@@ -1,8 +1,39 @@
 package com.company;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Lexer {
-    private static String[] keywords = new String[]();
-    
+    private static ArrayList<String> keywordsList = new ArrayList<>();
+    private static ArrayList<String> symbolsList = new ArrayList<>();
+
+    static void preProcess(){
+        String tokenTypesDirectory = System.getProperty("user.dir") + "\\src\\com\\company\\InputFiles\\Token Types";
+        try {
+            addContentToList(keywordsList, tokenTypesDirectory + "\\KEYWORDS.txt");
+            addContentToList(symbolsList, tokenTypesDirectory + "\\SYMBOLS.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void addContentToList(List<String> list, String directory) throws FileNotFoundException {
+        FileInputStream inKeywords = new FileInputStream(directory);
+        Scanner scanner = new Scanner(inKeywords);
+        while(scanner.hasNext())
+            keywordsList.add(scanner.next());
+    }
+
+    public static ArrayList<String> getKeywordsList() {
+        return keywordsList;
+    }
+
+    public static ArrayList<String> getSymbolsList() {
+        return symbolsList;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Lexer.preProcess();
     }
 
     //public static tok
