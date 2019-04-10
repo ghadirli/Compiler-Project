a52883c736ce11625fccc46bf641a555abb530ba
ghadirli <ghadirli@ce.sharif.edu>
2019-04-10 14:37:19 +0430

    function for reading input from file to string added

--- src/com/company/Lexer.java
+++ src/com/company/Lexer.java
@@ -1,8 +1,42 @@
 package com.company;
 
-import java.io.File;
+import java.io.*;
 
 public class Lexer {
     private static String[] keywords = new String[]();
-    
+    private String input;
+    private String inputFilePath;
+
+    public String readInputFromFile(String fileName) {
+        InputStream is = null;
+        try {
+
+            is = new FileInputStream(fileName);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        BufferedReader buf = new BufferedReader(new InputStreamReader(is));
+        String line = null;
+        try {
+            line = buf.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        StringBuilder sb = new StringBuilder();
+        while (line != null) {
+            sb.append(line).append("\n");
+            try {
+                line = buf.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        String fileAsString = sb.toString();
+
+        return fileAsString;
+
+    }
+
+
+
 }
