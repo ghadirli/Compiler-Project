d3d6428248508835cc06bb81b689cfb46eba6f85
Soroush <soroushtaslimi@yahoo.com>
2019-06-30 16:19:25 +0430

    break is being handled

--- src/com/company/Parser/Subroutines.java
+++ src/com/company/Parser/Subroutines.java
@@ -7,7 +7,8 @@ public class Subroutines {
     private Logger logger;
     private ArrayList<Integer> semanticStack = new ArrayList<>();
     private int pbLineNumber;
-    ArrayList<String> programBlock = new ArrayList<>();
+    private ArrayList<String> programBlock = new ArrayList<>();
+    private ArrayList<ArrayList<Integer>> breakLines = new ArrayList<>();
     public Subroutines(Logger logger){
         this.logger = logger;
     }
@@ -23,7 +24,26 @@ public class Subroutines {
             case "#while":
                 while0();
                 break;
+            case "#ifsjpf_save":
+                ifsjps_save();
+                break;
+            case "#ifsjp":
+                ifsjp();
+                break;
+            case "#break":
+                break0();
+                break;
         }
+    }
+
+    private void popss(int numberToPop){
+        for (int i = 0; i < numberToPop; i++) {
+            semanticStack.remove(semanticStack.size()-1);
+        }
+    }
+
+    private void pushss(int val){
+        semanticStack.add(val);
     }
 
     private void incrementPBLine(){
@@ -31,21 +51,47 @@ public class Subroutines {
         programBlock.add("");
     }
 
+    private int ssFromLast(int bias){
+        return semanticStack.get(semanticStack.size()-1-bias);
+    }
+
+
+
+    //------------------------------subroutines----------------------------------------
+
     private void label(){
-        semanticStack.add(pbLineNumber);
+        pushss(pbLineNumber);
     }
 
     private void save(){
-        semanticStack.add(pbLineNumber);
+        pushss(pbLineNumber);
         incrementPBLine();
     }
 
     private void while0(){
-        programBlock.set(semanticStack.get(semanticStack.size()-1), "(JPF, " + semanticStack.get(semanticStack.size()-2) + ", " + (pbLineNumber + 1) + ", )");
+        programBlock.set(ssFromLast(0), "(JPF, " + ssFromLast(1) + ", " + (pbLineNumber + 1) + ", )");
         programBlock.set(pbLineNumber, "(JP, " + semanticStack.get(semanticStack.size()-3) + ", , )");
         incrementPBLine();
-        for (int i = 0; i < 3; i++) {
-            semanticStack.remove(semanticStack.size()-1);
-        }
+        popss(3);
+    }
+
+    private void ifsjps_save(){
+        programBlock.set(ssFromLast(0), "JPF, " + ssFromLast(1) + ", " + pbLineNumber+1 + ", )");
+        popss(2);
+        pushss(pbLineNumber+1);
+        incrementPBLine();
+    }
+
+    private void ifsjp(){
+        programBlock.set(ssFromLast(0), "(JP, " + pbLineNumber + ", , )");
+        popss(1);
+    }
+
+    private void new_breakable(){
+
+    }
+
+    private void break0(){
+
     }
 }
